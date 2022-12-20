03cb5e0a3542b4192ea7637331b69144d085d29b
aturaal <116278053+aturaal@users.noreply.github.com>
2022-12-20 09:17:24 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const { Console } = require('console');
 const express = require('express')
 const app = express();
-const fileUpload = require('express-fileupload')
 const path = require('path')
-const csv = require('csv-parser')
-const fs = require('fs')
+const multer = require('multer');
+const fs = require('fs');
+const { json } = require('express');
 const results = []
+const upload = multer();
+const papa = require('papaparse');
+const csvFilePath = 'files/csvfile.csv'
+const { emitWarning } = require('process');
+const knex = require("knex");
+const bodyParser = require("body-parser");
+const checkResults = []
+
+app.use(bodyParser.json({
+  limit: "11MB"
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const server = require("http").createServer(app);
+
+
+
+const database = knex({
+  client: "postgresql",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    password: "annen12",
+    database: "postgres",
+    port: "5432",
+  },
+  useNullAsDefault: true,
+});
+
+const router = express.Router();
+module.exports = router;
+
 
 
 
@@ -13,31 +45,46 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "html.html"));
 });
 
+
+
 app.post('/upload',
-    fileUpload({ createParentPath: true }),
+    upload.any('files', 10),
     (req, res) => {
+    	
         const files = req.files
-        console.log(files)
+        files.map((file, index) => {
+          let csv = new Buffer(files[0].buffer, 'base64').toString('ascii');
+          fs.writeFile('files/csvfile.csv' , files[0].buffer, 'utf8' , function(err, files) {
+            if (err) console.log('error', err);
 
-        Object.keys(files).forEach(key => {
-            const filepath = path.join(__dirname, 'files', files[key].name)
-            files[key].mv(filepath, (err) => {
-                if (err) return res.status(500).json({ status: "error", message: err })
-            })
+          })
+          
+          const content = file.buffer.toString("utf-8");
+          
+          papa.parse(content, {
+	    header: true,
+	    complete: results => {
+	      console.log (content)
+	    }
+      
+          });
         })
-        
-    
-        
-
-        return res.json({ status: 'success', message: results , message: Object.keys(files).toString() })
-     
 
     }
+    
 )
+
+app.get('/deneme' , (req, res) => {
+
+  res.send(results)
+})
+
+
+
 
 const PORT = process.env.PORT || 3110
 
-app.listen(PORT, () => console.log(`This seğver is listening on: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
